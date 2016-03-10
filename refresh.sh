117d93d668a911f7dafe2af64928f90ecd09b123
scotthaleen <scott.haleen@gmail.com>
2016-03-10 13:27:25 -0500

    added refresh script for repo

---
+++ refresh.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+
+cp ~/.bash_profile bash_profile
+
+cp ~/.bashrc bashrc
+
+cp ~/.emacs_bash emacs_bash
+
+cp -R ~/.fonts/* fonts/
+
+cp ~/.profile profile
+
+cp -R ~/.profile.d/* profile.d/
+
+cp ~/.tmux.conf tmux.conf
+
+cp ~/.zshrc zshrc
+
